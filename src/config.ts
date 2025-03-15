d4e996ef56afd71789841eba1abdfcc2e8069794
Gwenitora <gwenitora.gardu74@gmail.com>
2025-03-15 18:28:55 +0100

    fix: update default config

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,24 @@
-import { type SomeCompanionConfigField } from '@companion-module/base'
+import { CompanionConfigField, CompanionInputFieldStaticText, type SomeCompanionConfigField } from '@companion-module/base'
 import { ModuleInstance } from './main.js';
 export interface ModuleConfig {}
 
-export function GetConfigFields(self: ModuleInstance): SomeCompanionConfigField[] {
+export const GetConfigFields = (self: ModuleInstance): SomeCompanionConfigField[] => {
 	var out: SomeCompanionConfigField[] = [];
-
 	self;
 
+	out.push(GenerateSeperation())
+
 	return out;
+}
+
+var sep = -1;
+const GenerateSeperation = (): CompanionInputFieldStaticText & CompanionConfigField => {
+	sep++;
+	return {
+		type: 'static-text',
+		id: 'seperator_' + sep,
+		label: '',
+		width: 12,
+		value: '<hr />'
+	}
 }
